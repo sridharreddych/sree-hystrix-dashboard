e56293acceacbd36889e06b5dce37fef0341fea9
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-03-19 17:29:55 +0530

    first commit

---
+++ src/main/java/sree/demo/SreeHystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package sree.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SreeHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreeHystrixDashboardApplication.class, args);
+	}
+
+}
